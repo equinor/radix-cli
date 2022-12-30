9d2f8dd8c36d91dc89bdf0a6e38b3d61e7498617
Sergey Smolnikov <ssmol@equinor.com>
2022-12-30 23:32:27 +0100

    Update cmd/promoteApplication.go
    Co-authored-by: Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>

--- cmd/promoteApplication.go
+++ cmd/promoteApplication.go
@@ -38,7 +38,7 @@ var promoteApplicationCmd = &cobra.Command{
 
 		deploymentName, _ := cmd.Flags().GetString("deployment")
 		fromEnvironment, _ := cmd.Flags().GetString("from-environment")
-		toEnvironment, _ := cmd.Flags().GetString("to")
+		toEnvironment, _ := cmd.Flags().GetString("to-environment")
 		triggeredByUser, _ := cmd.Flags().GetString("user")
 		follow, _ := cmd.Flags().GetBool("follow")
 
