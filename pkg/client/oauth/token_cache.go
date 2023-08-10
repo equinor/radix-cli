40230beb655b97f1e7913fbb9be1d057e0dc2a0a
Sergey Smolnikov <ssmol@equinor.com>
2023-08-10 17:04:55 +0200

    Saved contract to contract file

--- pkg/client/oauth/token_cache.go
+++ pkg/client/oauth/token_cache.go
@@ -5,6 +5,7 @@ import (
 	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/cache"
 	customJSON "github.com/equinor/radix-cli/pkg/client/oauth/internal/json"
 	radixconfig "github.com/equinor/radix-cli/pkg/config"
+	jsonutils "github.com/equinor/radix-cli/pkg/utils/json"
 	log "github.com/sirupsen/logrus"
 	"os"
 )
@@ -16,7 +17,7 @@ type TokenCache struct {
 
 func NewTokenCache(radixConfig *radixconfig.RadixConfig) *TokenCache {
 	return &TokenCache{
-		file:        radixconfig.RecommendedHomeMsalCredsFile,
+		file:        radixconfig.RecommendedHomeMsalContractFile,
 		radixConfig: radixConfig,
 	}
 }
@@ -30,7 +31,7 @@ func (t *TokenCache) Replace(ctx context.Context, cache cache.Unmarshaler, hints
 	if err := customJSON.Unmarshal(data, contract); err != nil {
 		return err
 	}
-	t.radixConfig.MsalContract = contract
+	t.radixConfig.MSALContract = contract
 	return cache.Unmarshal(data)
 }
 
@@ -39,14 +40,31 @@ func (t *TokenCache) Export(ctx context.Context, cache cache.Marshaler, hints ca
 	if err != nil {
 		log.Println(err)
 	}
-	contract := radixconfig.NewContract()
-	if err := customJSON.Unmarshal(data, contract); err != nil {
+	msalContract := radixconfig.NewContract()
+	if err := customJSON.Unmarshal(data, msalContract); err != nil {
 		return err
 	}
-	t.radixConfig.MsalContract = contract
+	t.radixConfig.MSALContract = msalContract
 	err = radixconfig.Save(t.radixConfig)
 	if err != nil {
 		return err
 	}
+	err = ensureMsalContractFileExists()
+	if err != nil {
+		return err
+	}
 	return os.WriteFile(t.file, data, 0600)
+}
+
+func ensureMsalContractFileExists() error {
+	if _, err := os.Stat(radixconfig.RecommendedConfigDir); os.IsNotExist(err) {
+		err := os.MkdirAll(radixconfig.RecommendedConfigDir, os.ModePerm)
+		if err != nil {
+			return err
+		}
+	}
+	if _, err := os.Stat(radixconfig.RecommendedHomeMsalContractFile); err == nil {
+		return jsonutils.Save(radixconfig.RecommendedHomeMsalContractFile, radixconfig.NewContract())
+	}
+	return nil
 }
--- pkg/config/radix_config.go
+++ pkg/config/radix_config.go
@@ -18,9 +18,9 @@ const (
 	ContextDevelopment = "development"
 	ContextPlatform2   = "platform2"
 
-	recommendedHomeDir           = ".radix"
-	recommendedFileName          = "config"
-	recommendedMsalCredsFileName = "creds"
+	recommendedHomeDir              = ".radix"
+	recommendedFileName             = "config"
+	recommendedMsalContractFileName = "contract"
 
 	clientID    = "ed6cb804-8193-4e55-9d3d-8b88688482b3"
 	tenantID    = "3aa4a235-b6e2-48d5-9195-7fcf05b459b0"
@@ -32,21 +32,28 @@ const (
 )
 
 var (
-	RecommendedConfigDir         = path.Join(homedir.HomeDir(), recommendedHomeDir)
-	RecommendedHomeFile          = path.Join(RecommendedConfigDir, recommendedFileName)
-	RecommendedHomeMsalCredsFile = path.Join(RecommendedConfigDir, recommendedMsalCredsFileName)
-	ValidContexts                = []string{ContextProduction, ContextPlatform, ContextPlatform2, ContextPlayground, ContextDevelopment}
+	RecommendedConfigDir            = path.Join(homedir.HomeDir(), recommendedHomeDir)
+	RecommendedHomeFile             = path.Join(RecommendedConfigDir, recommendedFileName)
+	RecommendedHomeMsalContractFile = path.Join(RecommendedConfigDir, recommendedMsalContractFileName)
+	ValidContexts                   = []string{ContextProduction, ContextPlatform, ContextPlatform2, ContextPlayground, ContextDevelopment}
 )
 
 type RadixConfig struct {
+	// CustomConfig is the custom environment config
 	CustomConfig *CustomConfig `json:"customConfig"`
-	ClientID     string        `json:"-"`
-	TenantID     string        `json:"-"`
-	APIServerID  string        `json:"-"`
-	MsalContract *Contract     `json:"-"`
+	// ClientID is the ID of the Azure AD app registration
+	ClientID string `json:"-"`
+	// TenantID is the ID of the tenant that the client will authenticate with
+	TenantID string `json:"-"`
+	// APIServerID is the ID of the API server that the client will use to get tokens
+	APIServerID string `json:"-"`
+	// MSALContract is the MSAL internal structure that is written to any storage medium when serializing the cache
+	MSALContract *Contract `json:"-"`
 }
 
+// CustomConfig is the custom environment config
 type CustomConfig struct {
+	// Context is the environment context: platform (default), playground, development, platform2
 	Context string `json:"Context"`
 }
 
@@ -87,7 +94,7 @@ func GetDefaultRadixConfig() *RadixConfig {
 		ClientID:     clientID,
 		TenantID:     tenantID,
 		APIServerID:  apiServerID,
-		MsalContract: NewContract(),
+		MSALContract: NewContract(),
 	}
 }
 
@@ -154,15 +161,17 @@ func ToConfig(config map[string]string) *RadixConfig {
 		}
 	}
 
-	return NewRadixConfig(customConfig)
+	radixConfig := NewRadixConfig()
+	radixConfig.CustomConfig = customConfig
+	return radixConfig
 }
 
-func NewRadixConfig(config *CustomConfig) *RadixConfig {
+func NewRadixConfig() *RadixConfig {
 	return &RadixConfig{
-		CustomConfig: config,
+		CustomConfig: &CustomConfig{},
 		ClientID:     clientID,
 		TenantID:     tenantID,
 		APIServerID:  apiServerID,
-		MsalContract: NewContract(),
+		MSALContract: NewContract(),
 	}
 }
