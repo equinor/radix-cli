7336d6d65b32995cd5c3048e079d1a5d08e6aaed
Richard Hagen <richard.hagen@bouvet.no>
2023-10-12 10:41:44 +0200

    read version from git tag

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	_ "embed"
 	"errors"
 	"fmt"
 	"os"
@@ -19,9 +20,11 @@ import (
 
 const (
 	radixCLIError = "Error: Radix CLI executed with error"
-	version       = "1.9.1"
 )
 
+//go:generate sh -c "printf %s $(git describe --tags) > tag.txt"
+//go:embed tag.txt
+var version string
 var rootLongHelp = strings.TrimSpace(`
 A command line interface which allows you to interact with the Radix platform through automation.
 `)
@@ -31,7 +34,7 @@ var rootCmd = &cobra.Command{
 	Use:     "rx",
 	Short:   "Command line interface for Radix platform",
 	Long:    rootLongHelp,
-	Version: version,
+	Version: "version: " + version,
 }
 
 // Execute the top level command
