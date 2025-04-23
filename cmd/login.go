3e9f1dc4c86ef2d6179f6512c441d556fd26d642
Richard87 <richard.hagen@gmail.com>
2025-04-23 09:12:43 +0200

    fix: authenticate with github federated credentials

--- cmd/login.go
+++ cmd/login.go
@@ -16,7 +16,7 @@ package cmd
 
 import (
 	"context"
-	"errors"
+	"fmt"
 
 	"github.com/equinor/radix-cli/pkg/client"
 	"github.com/equinor/radix-cli/pkg/flagnames"
@@ -24,7 +24,7 @@ import (
 )
 
 var (
-	errInvalidAzureClientFlags = errors.New("azure client id must be used together with azure client secret or federated token")
+	errInvalidAzureClientFlags = fmt.Errorf("%s must be used together with %s, %s or %s", flagnames.AzureClientId, flagnames.AzureClientSecret, flagnames.FederatedTokenFile, flagnames.UseGithubCredentials)
 )
 
 // loginCmd represents the login command
@@ -42,7 +42,7 @@ var loginCmd = &cobra.Command{
 		azureClientId, _ := cmd.Flags().GetString(flagnames.AzureClientId)
 		azureClientSecret, _ := cmd.Flags().GetString(flagnames.AzureClientSecret)
 
-		if !useGithubCredentials && azureClientId != "" && azureClientSecret == "" && federatedTokenFile == "" {
+		if azureClientId != "" && !useGithubCredentials && azureClientSecret == "" && federatedTokenFile == "" {
 			return errInvalidAzureClientFlags
 		}
 		if !useInteractiveLogin && !useDeviceCode && !useGithubCredentials && azureClientId == "" {
