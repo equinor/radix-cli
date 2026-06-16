c3b750f6bc515d5b8135b56a5e601ec89a5b5803
Nils Gustav Stråbø <65334626+nilsgstrabo@users.noreply.github.com>
2026-06-16 16:21:52 +0200

    sort scopes when creating cache key

--- pkg/auth/utils.go
+++ pkg/auth/utils.go
@@ -2,9 +2,12 @@ package auth
 
 import (
 	"fmt"
+	"slices"
 	"strings"
 )
 
 func accessTokenCacheKey(scopes []string) string {
-	return fmt.Sprintf("access_token-%s", strings.Join(scopes, " "))
+	clonedScopes := slices.Clone(scopes)
+	slices.Sort(clonedScopes)
+	return fmt.Sprintf("access_token-%s", strings.Join(clonedScopes, " "))
 }
