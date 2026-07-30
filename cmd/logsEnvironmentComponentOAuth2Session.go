42010b2fde855415baafc50fa267be06ad12b9ef
herda1 <hera@equinor.com>
2026-07-30 10:24:35 +0200

    add aux oauth2 logs in cli

---
+++ cmd/logsEnvironmentComponentOAuth2Session.go
@@ -0,0 +1,91 @@
+// Copyright © 2023
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"errors"
+
+	"github.com/equinor/radix-cli/pkg/client"
+	"github.com/equinor/radix-cli/pkg/config"
+	"github.com/equinor/radix-cli/pkg/flagnames"
+	"github.com/equinor/radix-cli/pkg/settings"
+	"github.com/equinor/radix-cli/pkg/utils/completion"
+	"github.com/equinor/radix-cli/pkg/utils/replicalog"
+	"github.com/spf13/cobra"
+)
+
+// logsEnvironmentComponentOAuthSessionCmd represents the logsEnvironmentComponentOAuthSessionCmd command
+var logsEnvironmentComponentOAuth2SessionCmd = &cobra.Command{
+	Use:   "oauth2-session",
+	Short: "Gets logs for the OAuth2 session store (Redis) auxiliary resource of a component",
+	Long: `Gets and follows logs for the OAuth2 session store (Redis) auxiliary resource of a component in an environment.
+
+It may take few seconds to get the log.
+
+Examples:
+  # Get logs for the OAuth2 session store auxiliary resource of a component
+  rx get logs component oauth2-session --application radix-test --environment dev --component web-app
+
+  # Short version
+  rx get logs component oauth2-session -a radix-test -e dev --component web-app
+`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		appName, err := config.GetAppNameFromConfigOrFromParameter(cmd, flagnames.Application)
+		if err != nil {
+			return err
+		}
+
+		if appName == "" {
+			return errors.New("application name is required")
+		}
+
+		environmentName, _ := cmd.Flags().GetString(flagnames.Environment)
+		componentName, _ := cmd.Flags().GetString(flagnames.Component)
+		previousLog, _ := cmd.Flags().GetBool(flagnames.Previous)
+		since, _ := cmd.Flags().GetDuration(flagnames.Since)
+
+		if environmentName == "" || componentName == "" {
+			return errors.New("both `environment` and `component` are required")
+		}
+
+		cmd.SilenceUsage = true
+
+		apiClient, err := client.GetRadixApiForCommand(cmd)
+		if err != nil {
+			return err
+		}
+
+		return replicalog.New(
+			cmd.ErrOrStderr(),
+			replicalog.GetReplicasForComponentOAuth2(apiClient, appName, environmentName, componentName, "oauth-redis", previousLog),
+			replicalog.GetOAuth2ComponentLog(apiClient, appName, previousLog),
+			since,
+		).StreamLogs(cmd.Context(), false)
+	},
+}
+
+func init() {
+	logsEnvironmentComponentCmd.AddCommand(logsEnvironmentComponentOAuth2SessionCmd)
+	logsEnvironmentComponentOAuth2SessionCmd.Flags().StringP(flagnames.Application, "a", "", "Name of the application owning the component")
+	logsEnvironmentComponentOAuth2SessionCmd.Flags().StringP(flagnames.Environment, "e", "", "Environment the component runs in")
+	logsEnvironmentComponentOAuth2SessionCmd.Flags().String(flagnames.Component, "", "The component to follow")
+	logsEnvironmentComponentOAuth2SessionCmd.Flags().BoolP(flagnames.Previous, "p", false, "If set, print the logs for the previous instance of the OAuth2 session store auxiliary container, if it exists")
+	logsEnvironmentComponentOAuth2SessionCmd.Flags().DurationP(flagnames.Since, "s", settings.DeltaRefreshApplication, "If set, start get logs from the specified time, eg. 5m or 12h")
+
+	_ = logsEnvironmentComponentOAuth2SessionCmd.RegisterFlagCompletionFunc(flagnames.Application, completion.ApplicationCompletion)
+	_ = logsEnvironmentComponentOAuth2SessionCmd.RegisterFlagCompletionFunc(flagnames.Environment, completion.EnvironmentCompletion)
+	_ = logsEnvironmentComponentOAuth2SessionCmd.RegisterFlagCompletionFunc(flagnames.Component, completion.ComponentCompletion)
+	setContextSpecificPersistentFlags(logsEnvironmentComponentOAuth2SessionCmd)
+}
--- pkg/utils/replicalog/component.go
+++ pkg/utils/replicalog/component.go
@@ -97,6 +97,84 @@ func GetComponentReplicasForEnvironment(apiClient *radixapi.Radixapi, appName, e
 	}
 }
 
+type OAuth2ComponentItem struct {
+	Component string
+	Env       string
+	Replica   string
+	AuxType   string
+	created   time.Time
+}
+
+func (c OAuth2ComponentItem) Identifier() string {
+	return c.Component + "/" + c.AuxType + "/" + c.Replica
+}
+
+func (c OAuth2ComponentItem) Created() time.Time {
+	return c.created
+}
+
+func GetReplicasForComponentOAuth2(apiClient *radixapi.Radixapi, appName, environmentName, componentName, auxType string, previousLog bool) GetReplicasFunc[OAuth2ComponentItem] {
+	return func() ([]OAuth2ComponentItem, bool, error) {
+		environmentParams := environment.NewGetEnvironmentParams()
+		environmentParams.SetAppName(appName)
+		environmentParams.SetEnvName(environmentName)
+		environmentDetails, err := apiClient.Environment.GetEnvironment(environmentParams, nil)
+
+		if err != nil {
+			return nil, false, err
+		}
+
+		if environmentDetails == nil || environmentDetails.Payload.ActiveDeployment == nil {
+			return nil, false, errors.New("active deployment was not found in environment")
+		}
+
+		var replicas []OAuth2ComponentItem
+		for _, comp := range environmentDetails.Payload.ActiveDeployment.Components {
+			if comp.Name == nil || *comp.Name != componentName {
+				continue
+			}
+			if comp.Oauth2 != nil {
+				for _, deployment := range comp.Oauth2.Deployments {
+					if deployment.Type != auxType {
+						continue
+					}
+					for _, replica := range deployment.ReplicaList {
+						replicas = append(replicas, OAuth2ComponentItem{
+							Component: *comp.Name,
+							Env:       environmentName,
+							Replica:   *replica.Name,
+							AuxType:   deployment.Type,
+							created:   time.Time(*replica.Created),
+						})
+					}
+				}
+			}
+			break
+		}
+
+		return replicas, previousLog, nil
+	}
+}
+
+func GetOAuth2ComponentLog(apiClient *radixapi.Radixapi, appName string, previous bool) GetLogFunc[OAuth2ComponentItem] {
+	previousStr := strconv.FormatBool(previous)
+
+	return func(ctx context.Context, item OAuth2ComponentItem, since time.Time, callback consumer.EventCallback) error {
+		logParameters := component.NewGetOAuthPodLogParamsWithContext(ctx)
+		logParameters.WithAppName(appName)
+		logParameters.WithEnvName(item.Env)
+		logParameters.WithComponentName(item.Component)
+		logParameters.WithPodName(item.Replica)
+		logParameters.WithType(item.AuxType)
+		logParameters.WithFollow(pointers.Ptr("true"))
+		logParameters.WithSinceTime(pointers.Ptr(strfmt.DateTime(since)))
+		logParameters.WithPrevious(&previousStr)
+
+		_, err := apiClient.Component.GetOAuthPodLog(logParameters, nil, consumer.NewEventSourceClientOptions(callback))
+		return err
+	}
+}
+
 func GetComponentLog(apiClient *radixapi.Radixapi, appName string, previous bool) GetLogFunc[ComponentItem] {
 	previousStr := strconv.FormatBool(previous)
 
